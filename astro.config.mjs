140273e37dcda4b5b71c4ce91912ab24412f67d4
Hamza <144457566+unk-user@users.noreply.github.com>
2024-10-15 20:59:49 +0100

    add web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [tailwind(), react()],
 });
